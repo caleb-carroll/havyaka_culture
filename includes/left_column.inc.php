c2b7d110f44674678c3af8ce846519c07a2ffccd
nivedita-bhat <nivi@iastate.edu>
2014-03-11 11:34:42 -0500

    basic navigation and Home page
    Added basic css and created 2 column layout. Added basic format of
    cards. We can make changes tomorrow.

---
+++ includes/left_column.inc.php
@@ -0,0 +1,38 @@
+<!-- Column 1 start -->
+<div id = "left_column">	
+    <div class="category_heading">
+	<center>Food Categories</center>
+    
+        <div class="categories">
+            <p>
+            <ul>
+                <li>
+                    Food1
+                </li>
+                <li>
+                    Food2
+                </li>
+                <li>
+                    Food3
+                </li>
+                <li>
+                    Food4
+                </li>
+                <li>
+                    Food5
+                </li>
+                
+            </ul>
+                
+            </p>
+          
+        </div>			
+    </div>			
+    <div class="ad_search_heading">
+            <center>Advanced Search</center>
+    
+        <div class="search_option">
+            <p>search options</p>
+        </div>
+    </div>
+</div>		
--- index.php
+++ index.php
@@ -1,3 +1,70 @@
-<h1>This is the entry page for Havyaka Culture Site</h1>
-<h2>This is a sub-header</h2>
-<h3>This is a sub-sub-header</h3>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-GB">
+<head>
+	<title>Community Resource</title>
+	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+	<meta name="robots" content="index, follow" />
+        <link rel="stylesheet" type="text/css" href="includes/styles/style.css" media="screen" />
+</head>
+    <body>
+        <div id ="header">
+            <h1>Community Connect</h1> 
+      
+        
+        <?php include('includes/navigation.inc.php'); ?>
+       </div>  
+        
+        
+  <div class="content leftmenu">      
+        
+      <div class="colright">
+		<div class="col1">
+			<!-- Column 1 start -->
+			<?php include('includes/left_column.inc.php'); ?>			
+			
+			<!-- Column 1 end -->
+		</div>
+		<div class="col2">
+                    <!-- Column 2 start -->
+                        <div id="carousal">
+                          <p>random images goes here</p>
+               
+                        </div>
+                        <div class ="card">
+
+                            <h1>This is a card!</h1>
+                                <p>In essence, a card is just a rectangular region which contains content.  This content could be text, images, lists, etc...
+                                    The card UI methaphor dictates the interaction and layout of these regions.</p>
+                        </div>
+                        <div class="card card-back">                        
+                                <p>Contents for card1 goes here</p>
+                        </div>
+                        <div class="card card-image">
+                                 <p>Contents for card2 goes here</p>
+                        </div> 
+                        <div class="card card-image">
+                                 <p>Contents for card3 goes here</p>
+                        </div> 
+                        <div class="card card-image">
+                                 <p>Contents for card4 goes here</p>
+                        </div> 
+                        <div class="card card-image">
+                                 <p>Contents for card5 goes here</p>
+                        </div> 
+
+                                <!-- Column 2 end -->
+                 </div>
+               <!-- for future reference 
+               <div class="col3"> 
+                   
+                </div>
+               -->
+       </div>
+
+</div>
+
+<?php include('includes/footer.inc.php'); ?>
+
+</body>
+</html>
+    
