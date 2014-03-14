6e7fdfbe1a46acca92bb7f216dc6bf35588aec19
caleb-carroll <caleb.m.carroll@gmail.com>
2014-03-13 21:56:24 -0500

    Install DB fixed

--- includes/constants/dbc.php
+++ includes/constants/dbc.php
@@ -1,9 +1,9 @@
 <?php
 
 //base in operating system
-define ("SITE_ROOT", $_SERVER['DOCUMENT_ROOT'] . "/Havyaka_community_project/havyaka_culture");
+define ("ROOT", $_SERVER['DOCUMENT_ROOT'] . "/havyaka_culture");
 
 //base URL of site
-define ("SITE_BASE", "http://".$_SERVER['HTTP_HOST']."/Havyaka_community_project/havyaka_culture");
-echo SITE_ROOT;
+define ("BASE", "http://".$_SERVER['HTTP_HOST']."/havyaka_culture");
+// echo SITE_ROOT;
 ?>
